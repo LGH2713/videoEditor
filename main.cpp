7ffb1272039933182d8fcb6aeb17242bf2498117
lgh <2524398864@qq.com>
2022-10-09 14:11:59 +0800

    feat: 配置ffmpeg、sdl

--- main.cpp
+++ main.cpp
@@ -2,20 +2,10 @@
 
 #include <QApplication>
 
-extern "C" {
-#include "SDL.h"
-#undef main
-}
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    if(SDL_Init(SDL_INIT_VIDEO)) {
-        printf( "Could not initialize SDL - %s\n", SDL_GetError());
-    } else{
-        printf("Success init SDL");
-    }
     return 0;
     w.show();
     return a.exec();
